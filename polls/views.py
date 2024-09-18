ff021f9cea54f613475784822af68d27b5a4fd4f
AdalidH1 <118926555+AdalidH1@users.noreply.github.com>
2024-09-17 18:34:43 -0600

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+import os
+import json
 
 from .models import Question
 
-
+def manifest(request):
+    # Ruta absoluta al archivo manifest.json (ajusta según la estructura de tu proyecto)
+    manifest_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'manifest.json')
+    
+    try:
+        # Lee el archivo y devuelve su contenido como JSON
+        with open(manifest_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)  # Carga el archivo como JSON
+        
+        # Devuelve la respuesta JSON
+        return JsonResponse(data, safe=False)
+    
+    except FileNotFoundError:
+        # Si el archivo no se encuentra, devuelve un error 404
+        return HttpResponse("Archivo manifest.json no encontrado", status=404)
+    
+    except json.JSONDecodeError:
+        # Si hay un error en el formato del JSON, devuelve un error 500
+        return HttpResponse("Error al decodificar manifest.json", status=500)
+        
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
