ff9dd5b3752ba0673ea60455dc47a6b155a17588
Mafe-SL <fernanda.mariastb@gmail.com>
2024-09-17 18:45:46 -0600

    manifest

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from polls import views
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-     path('manifest.json', views.manifest, name='manifest'),
+    path("", views.index),
+    path('manifest.json', views.manifest, name='manifest'),
 ]
